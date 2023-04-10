9c0251bfc322c92c73eae01aad5f39876b1fe0ba
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-10 21:08:35 +0200

    good to go

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 // connecting to MongoDB
@@ -25,6 +26,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 // ROUTING 
 
@@ -47,7 +49,7 @@ app.get('/campgrounds/new', (req, res) => {
 app.post('/campgrounds', async (req, res) => {
     const campground = new Campground(req.body);
     await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
+   
 })
 
 
@@ -55,6 +57,17 @@ app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/show", {campground})
 });
+ res.redirect(`/campgrounds/${campground._id}`);
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", {campground})
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+  res.redirect(`/campgrounds/${campground._id}`);
+})
 
 
 app.listen(4000, () => {
