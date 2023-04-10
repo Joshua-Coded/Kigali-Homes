b0419fb9d34df8000d92d40e6c85058dc721e967
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-10 19:41:59 +0200

    show page

--- index.js
+++ index.js
@@ -37,6 +37,11 @@ app.get('/campgrounds', async (req, res) => {
 })
 
 
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/show", {campground})
+});
+
 
 
 app.listen(4000, () => {
