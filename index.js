8737c0633b62b12259939d37dad425c72e0ea66a
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-10 13:48:01 +0200

    initializing the app

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render("home");
+})
+
+
+app.listen(4000, () => {
+    console.log('listening on port 4000!');
+});
