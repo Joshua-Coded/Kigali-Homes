f3d2275d65d7422dbfef5d8d9bf7c4bd5b52730b
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-11 02:58:23 +0200

    all error fixed

--- index.js
+++ index.js
@@ -48,11 +48,10 @@ app.get('/campgrounds/new', (req, res) => {
 
 
 app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body);
+    const campground = new Campground(req.body.campground);
     await campground.save();
-   
+    res.redirect(`/campgrounds/${campground._id}`)
 })
-
 
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
@@ -67,16 +66,15 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
 
 app.put('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
-  const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
-  res.redirect(`/campgrounds/${campground._id}`);
-})
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
+});
 
 app.delete('/campgrounds/:id', async (req, res) => {
-    const { id } = req.body.params;
+    const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 })
-
 
 app.listen(4000, () => {
     console.log('listening on port 4000!');
