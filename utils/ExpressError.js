268580a9359889e08948584b04e314f78b8c7e0e
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-12 02:31:10 +0200

    catch error

---
+++ utils/ExpressError.js
@@ -0,0 +1,9 @@
+class ExpressEror extends Error {
+    constructor(message,statusCode) {
+        super();
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
+
+module.exports = ExpressEror;
