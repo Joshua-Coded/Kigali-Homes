cb893ebc83d13669b7d8d9af8f3e01d04d5fe2eb
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-17 13:25:38 +0200

    review model

--- models/campgrounds.js
+++ models/campgrounds.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const review = require('./review');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -6,7 +7,13 @@ const CampgroundSchema = new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
