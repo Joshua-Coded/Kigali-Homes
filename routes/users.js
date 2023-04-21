ebe6c4f56b1d79c63ad8bde710623ca28e8faaa9
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-21 16:40:09 +0200

    login routes

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express =  require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
+const passport = require('passport');
 
 router.get('/register', (req, res, next) => {
     res.render('users/register');
@@ -19,5 +20,16 @@ router.post('/register', catchAsync (async(req, res) => {
     res.redirect('register')
 }
 }))
+
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+
+});
+
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
+    req.flash('success', 'Welcome back!');
+    res.redirect('/campgrounds');
+});
 
 module.exports = router
