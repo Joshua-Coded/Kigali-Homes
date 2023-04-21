152b2369cb3df5741d2f6dc37b266310ff0208c4
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-21 18:03:55 +0200

    register routes fixed

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,16 @@ router.get('/register', (req, res, next) => {
     res.render('users/register');
 })
 
-router.post('/register', catchAsync (async(req, res) => {
+router.post('/register', catchAsync (async(req, res, next) => {
     try {
     const {email, username, password} = req.body;
     const user = new User({email, username });
     const registeredUser = await User.register(user, password);
-    req.flash('success', 'Welcome to Alu Camp');
+    req.login(registeredUser, err => {
+        if (err) return next(err);
+        req.flash('success', 'Welcome to Alu Camp');
     res.redirect('/campgrounds'); 
+     })  
 } catch(e) {
     req.flash('error', e.message);
     res.redirect('register')
