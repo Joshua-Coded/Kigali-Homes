41f95f5cfb4b8cabd8e727e9a14b3531447c8438
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-21 22:40:35 +0200

    authorisation

--- seeds/index.js
+++ seeds/index.js
@@ -44,6 +44,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '6442dcae4ffff4f75b52445c',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
