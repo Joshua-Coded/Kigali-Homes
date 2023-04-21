5f1714454eafe77ebbb1642ae48fba8615f991ea
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-21 15:01:22 +0200

    configuring passport

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 
 const campgrounds = require('./routes/campgrounds');
@@ -44,13 +47,31 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
+
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+})
+
+
+// fake users
+
+app.get('/fakeUser', async(req, res) => {
+    const user = new User({email: "joshua@gmail.com", username: "joshua"});
+    const newUser = await User.register(user, 'alana');
+    res.send(newUser);
+    
 })
 
 app.use('/campgrounds', campgrounds)
