32f9a816e95f008b64449098d6fdcc91f74cdeb8
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-21 15:45:19 +0200

    register form

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/alu-camp', {
     useNewUrlParser: true,
@@ -65,17 +65,10 @@ app.use((req, res, next) => {
 })
 
 
-// fake users
 
-app.get('/fakeUser', async(req, res) => {
-    const user = new User({email: "joshua@gmail.com", username: "joshua"});
-    const newUser = await User.register(user, 'alana');
-    res.send(newUser);
-    
-})
-
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+app.use('/', userRoutes)
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 app.get('/', (req, res) => {
     res.render('home')
