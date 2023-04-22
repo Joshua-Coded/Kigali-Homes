cdb3fd51d5234201830672cd33ca9f6cd550d85d
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-22 03:23:34 +0200

    authorisation

--- index.js
+++ index.js
@@ -87,6 +87,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
+
+
 app.listen(4000, () => {
     console.log('Serving on port 4000')
 })
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
-const { storeReturnTo } = require('../middleware');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -29,25 +28,17 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 })
 
-router.post('/login',
-    // use the storeReturnTo middleware to save the returnTo value from session to res.locals
-    storeReturnTo,
-    // passport.authenticate logs the user in and clears req.session
-    passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}),
-    // Now we can use res.locals.returnTo to redirect the user after login
-    (req, res) => {
-        req.flash('success', 'Welcome back!');
-        const redirectUrl = res.locals.returnTo || '/campgrounds'; // update this line to use res.locals.returnTo now
-        res.redirect(redirectUrl);
-    });
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'welcome back!');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+})
 
-
-router.get("/logout", (req, res) => {
-    req.logout(req.user, err => {
-      if(err) return next(err);
-      req.flash('success', "Goodbye!");
-      res.redirect('/campgrounds');
-    });
-  });
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', "Goodbye!");
+    res.redirect('/campgrounds');
+})
 
 module.exports = router;
