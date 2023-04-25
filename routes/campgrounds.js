70cb749d465c94d2014c4a747c19c0aa265c3991
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-25 03:17:53 +0200

    the multer middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,10 +5,15 @@ const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const Campground = require('../models/campgrounds');
 const campgrounds = require('../controllers/campgrounds');
 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(upload.array('image'), (req, res) => {
+        res.send(req.body, req.files)
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
