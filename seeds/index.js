5b751e05e24b944ec07bb1a90dedc1529237dd47
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-26 02:33:15 +0200

    fixing the seeds bug

--- seeds/index.js
+++ seeds/index.js
@@ -48,6 +48,13 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
+            geometry: { 
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+              },
             images: [
                 {
                   url: 'https://res.cloudinary.com/joshwilly/image/upload/v1682435564/AluCamp/odn27zeecngleovdmuow.png',
